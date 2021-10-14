d2a8c0883d5c73db0eefeaf9d4839fe1ea82f2a3
Orian <77785778+OrianP@users.noreply.github.com>
2021-10-14 23:01:05 +0100

    Intro screen Draft function to assign radio input values to variables relates #1

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ function assignRadioVals() {
     console.log({gameDuration, gameTone})
 }
 
+// create a function to convert variable name into string 
+
+// pass that string into getRadioVals and assign the result to the variable
+// pass the variables in an array to the assignRadioVals function
+
+function assignRadioVals(variables) {
+    // loop and assign each variable the result of calling getRadioVals on it's name
+}
+
 function getRadioVals(inputName) {
     return document.querySelector(`input[name="${inputName}"]:checked`).value;
 }
