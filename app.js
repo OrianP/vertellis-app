ca5212f27400799bb170812ed64efa65b0992536
Orian <77785778+OrianP@users.noreply.github.com>
2021-10-15 09:14:51 +0100

    Intro screen Initialize variables with a string equal to the input name attribute value

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
 // Intro screen
-let gameDuration = '';
-let gameTone = '';
+let gameDuration = 'game-duration';
+let gameTone = 'game-tone';
+
 const startBtn = document.querySelector('#game-start');
 
 // Event listeners
+
 startBtn.addEventListener('click', assignRadioVals);
 
 // Functions
 // think about how to abstract this further to take in any number of arguments and assign each with it's relevant radioVals
 function assignRadioVals() {
-    gameDuration = getRadioVals('game-duration');
-    gameTone = getRadioVals('game-tone');
+    gameDuration = getRadioVals(gameDuration);
+    gameTone = getRadioVals(gameTone);
     console.log({gameDuration, gameTone})
-}
-
-// create a function to convert variable name into string 
-
-// pass that string into getRadioVals and assign the result to the variable
-// pass the variables in an array to the assignRadioVals function
-
-function assignRadioVals(variables) {
-    // loop and assign each variable the result of calling getRadioVals on it's name
 }
 
 function getRadioVals(inputName) {
