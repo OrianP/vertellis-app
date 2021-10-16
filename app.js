c3650dda5ff6a2af1992eda2823ae6fa06c8a01c
Orian <77785778+OrianP@users.noreply.github.com>
2021-10-16 10:18:33 +0100

    Intro screen Replace players' select with two player name inputs. Add function to cget input values. relates #1

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 // Intro screen
+// players 1 and 2 names
+let p1 = 'p1';
+let p2 = 'p2';
 let gameDuration = 'game-duration';
 let gameTone = 'game-tone';
 
+// game start button
 const startBtn = document.querySelector('#game-start');
 
 // Event listeners
-
-startBtn.addEventListener('click', assignRadioVals);
+startBtn.addEventListener('click', assignVals);
 
 // Functions
 // think about how to abstract this further to take in any number of arguments and assign each with it's relevant radioVals
-function assignRadioVals() {
+function assignVals() {
+    p1 = getInputVals(p1);
+    p2 = getInputVals(p2);
     gameDuration = getRadioVals(gameDuration);
     gameTone = getRadioVals(gameTone);
-    console.log({gameDuration, gameTone})
+    console.log({p1, p2, gameDuration, gameTone})
 }
 
 function getRadioVals(inputName) {
     return document.querySelector(`input[name="${inputName}"]:checked`).value;
+}
+
+function getInputVals(inputName) {
+    return document.querySelector(`input[name="${inputName}"]`).value;
 }
